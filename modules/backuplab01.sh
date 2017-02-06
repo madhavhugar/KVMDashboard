ab3d6ff613da622415b05de05bec50b6946636cc
madhavhugar <madhav.hugar@gmail.com>
2017-02-06 21:56:07 +0100

    was created for test purposes

--- modules/backuplab01.sh
+++
@@ -1,8 +0,0 @@
-!/bin/bash
-
-result=$(ssh dsplab01 virsh dumpxml dsp-vm-student-tadasa \
-                | /usr/bin/awk -F: '{print "\""$1"\": \""$2"\"," }      '\
-                )
-
-echo "{" ${result%?} "}"
-
